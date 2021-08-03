4ed70e4f525fb80cc020f18b9ce7a7603f568817
elaquiambao <elanietquiambao@gmail.com>
2021-08-03 19:15:10 -0400

    Added to handle CORS

--- app/app.js
+++ app/app.js
@@ -18,6 +18,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 app.get("/", function (req, res) {
   res.send("Welcome to Trust Registry");
