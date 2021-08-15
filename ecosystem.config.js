fd71bd0726a3d7b06afd79f22f907ec4f601b478
elaquiambao <elanietquiambao@gmail.com>
2021-08-15 18:28:31 -0400

    added pm2 config file

---
+++ ecosystem.config.js
@@ -0,0 +1,41 @@
+module.exports = {
+  apps: [
+    {
+      name: "api-server",
+      cwd: `${__dirname}/app`,
+      script: "./bin/www",
+      watch: ["./app"],
+      env: {
+        NODE_ENV: "development",
+      },
+      env_production: {
+        NODE_ENV: "production",
+      },
+    },
+    {
+      name: "client-app",
+      cwd: `${__dirname}/client`,
+      script: "./node_modules/react-scripts/scripts/start.js",
+      watch: ["./client"],
+      env: {
+        NODE_ENV: "development",
+      },
+      env_production: {
+        NODE_ENV: "production",
+      },
+    },
+  ],
+
+  // deploy : {
+  //   production : {
+  //     user : 'SSH_USERNAME',
+  //     host : 'SSH_HOSTMACHINE',
+  //     ref  : 'origin/master',
+  //     repo : 'GIT_REPOSITORY',
+  //     path : 'DESTINATION_PATH',
+  //     'pre-deploy-local': '',
+  //     'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
+  //     'pre-setup': ''
+  //   }
+  // }
+};
