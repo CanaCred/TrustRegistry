7e9d2ca70188deee95e4859ce13a9caaf649ddc7
elaquiambao <elanietquiambao@gmail.com>
2021-09-29 20:17:53 -0400

    Update truffle-config.js

--- truffle-app/truffle-config.js
+++ truffle-app/truffle-config.js
@@ -1,18 +1,29 @@
 const path = require("path");
 const Provider = require("@truffle/hdwallet-provider");
-const privateKey =
-  "ae6ae8e5ccbfb04590405997ee2d52d2b330726137b875053c36d94e974d162f";
-const provider = new Provider(privateKey, "http://15.222.44.217:8545");
+/* besu */
+const provider = new Provider(
+  process.env.BESU_PRIVATE_KEY,
+  process.env.BESU_URL
+);
+/* rinkeby */
+// const provider = new Provider(process.env.PRIVATE_KEY, process.env.INFURA_URL);
 
 module.exports = {
   contracts_build_directory: path.join(__dirname, "build/contracts"),
-
+  /* besu */
   networks: {
     besuWallet: {
       provider: () => provider,
       network_id: "*",
     },
   },
+  /* rinkeby */
+  // networks: {
+  //   rinkeby: {
+  //     provider: () => provider,
+  //     network_id: "4",
+  //   },
+  // },
 
   // Set default mocha options here, use special reporters etc.
   mocha: {
